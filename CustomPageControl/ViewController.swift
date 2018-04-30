c594f429a41d67d60515c06c03d63d5191661011
Marcelo Sampaio <marcelo@artlogica.com.br>
2018-04-30 15:08:23 -0300

    initial commit 🔥🇧🇷

--- CustomPageControl/ViewController.swift
+++ CustomPageControl/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import FlexiblePageControl
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var flexiblePageControl: FlexiblePageControl!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        ////
+        
+        flexiblePageControl.numberOfPages = 10
+        
+        /////
+        flexiblePageControl.dotSize = 8
+        flexiblePageControl.dotSpace = 5
+        
+        flexiblePageControl.displayCount = 8
+        
+        flexiblePageControl.pageIndicatorTintColor = UIColor.gray
+        flexiblePageControl.currentPageIndicatorTintColor = UIColor.blue
+        
+        
+        
+
+        // Updated to the minimum size according to the displayCount
+        flexiblePageControl.updateViewSize()
+        
+        print("👍")
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
